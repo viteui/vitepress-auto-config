c1c6a30778f45cb014d3986cf04a71dcf87bb9b2
wcrane <1191814251@qq.com>
2024-11-24 12:48:50 +0800

    publish version 0.0.12

--- publish.config.js
+++ publish.config.js
@@ -3,9 +3,9 @@ module.exports = {
     // 发布目录
     root: ".",
     // 是否同步git
-    syncGit: false,
+    syncGit: true,
     // 是否同步git tag
-    syncGitTag: false,
+    syncGitTag: true,
     // 升级版本号的等级
     versionLevel: 'patch', // major | minor | patch
     // 自定义发布
